eefe45cb9dc55d23d133e2ac3887c32824e16483
nataszkan <natasza@fingerspitz.se>
2017-01-17 23:54:30 +0100

    starting out with eventhandler on button. onSubmit + input

--- src/App.js
+++ src/App.js
@@ -17,35 +17,37 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h4 className="date">Current time: {now.toTimeString()}</h4>
-        <Inputbox />
-        <Restaurants />
+        <AdressInputForm />
       </div>
     );
   }
 }
 
-class Restaurants extends React.Component {
+
+
+class AdressInputForm extends React.Component {
   render() {
-    const restaurants = ['Palmyra', 'Hjälmaren', 'Två små svin', 'Nelles'];
+
       return (
-      <div className="result-box">
-      <ul>
-{restaurants.map( rest => <li>{rest}</li> )}
-      </ul>
-      </div>
+          <form className="input-form" onSubmit={this._handleSearch.bind(this)}>
+            <label>Namnam hjälper dig att hitta en restaurang i närheten</label>
+              <div className="input-area">
+                <input placeholder="Skriv din adress tex. Sveavägen 83, Stockholm..." ref={(input) => this._address = input}/>
+                <button type="submit">sök</button>
+              </div>
+            </form>
+
     );
   }
+
+_handleSearch(event) {
+  event.preventDefault();
+
+  let address = this._address;
+  
+console.log("click Yes! addressen är: " + address);
 }
 
-class Inputbox extends React.Component {
-  render() {
-      return (
-        <div>
-        <input></input>
-        <button></button>
-        </div>
-    );
-  }
 }
 
 export default App;
