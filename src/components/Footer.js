8bab52075e8d89edc4a526228be5edb11e568488
Nina Juda <nina.juda@gmail.com>
2017-01-18 14:04:04 +0100

    Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () =>  {
+    return(
+        <header>
+            <div>
+                <div className="footerbox">
+                    <h4 className="footertext">
+                        App designed by Nina.J, Nina.M & Natasza.N
+                    </h4>
+                </div>
+            </div>
+        </header>
+
+    )
+}
+
+export default Footer;
