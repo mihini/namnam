895a9196857a4bc9d1d8070e66c894924dc6fe02
Nina Juda <nina.juda@gmail.com>
2017-01-18 14:03:47 +0100

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () =>  {
+    return(
+        <header>
+            <div className="headerbox">
+                <h1 className="headertext">
+                    NamNam
+                </h1>
+            </div>
+        </header>
+
+    )
+}
+
+export default Header;
