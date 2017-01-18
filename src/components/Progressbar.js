92bd9c5f3e796c1f1c50377902cbf9beebbb0423
Nina Juda <nina.juda@gmail.com>
2017-01-18 14:04:56 +0100

    Progressbar component

---
+++ src/components/Progressbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Progressbar = () =>  {
+    return(
+        <header>
+            <div className="progressbox">
+                <h1 className="progresstext">
+                    Progressbar
+                </h1>
+            </div>
+        </header>
+
+    )
+}
+
+export default Progressbar;
