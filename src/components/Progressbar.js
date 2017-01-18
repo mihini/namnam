f59564125efa2cb5830f4b45ada8b81a725aa7a5
Nina Juda <nina.juda@gmail.com>
2017-01-18 21:02:41 +0100

    Progressbar component (demo version with circle)

--- src/components/Progressbar.js
+++ src/components/Progressbar.js
@@ -1,16 +1,51 @@
 import React from 'react';
 
-const Progressbar = () =>  {
-    return(
-        <header>
-            <div className="progressbox">
-                <h1 className="progresstext">
-                    Progressbar
-                </h1>
-            </div>
-        </header>
+var ProgressBar = require('react-progressbar.js');
+var Circle = ProgressBar.Circle;
 
-    )
+class Progressbar extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            progress: 0.96,
+            text: "Hämtar käk"
+        }
+
+    }
+
+
+    render() {
+        var options = {
+            strokeWidth: 2,
+            duration: 1800
+        };
+
+
+        var containerStyle = {
+            width: '200px',
+            height: '200px'
+        };
+
+        return (
+
+        <div className="progressbar">
+
+            <Circle
+                progress={this.state.progress}
+                text={this.state.text}
+                options={options}
+                initialAnimate={true}
+                containerStyle={containerStyle}
+                containerClassName={'.progressbar'}
+            />
+        </div>
+        );
+    }
 }
+
+
+
 
 export default Progressbar;
