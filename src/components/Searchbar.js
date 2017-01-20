d4735d80594cb03299bb666584900c98b8ab237b
Nina Juda <nina.juda@gmail.com>
2017-01-20 09:51:04 +0100

    Fix: Searchbar functions

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 class Searchbar extends React.Component {
-    render() {
 
+    constructor(props){
+        super(props);
+        this.restaurang = this.restaurang.bind(this);
+
+    }
+
+
+    restaurang(){
+        const mintext = { inputText: this.inputText };
+
+        alert(this.inputText.value);
+    }
+
+    render() {
         return(
             <form className="searchbar-form">
-                <h1>Let us choose food for you</h1>
-                <input type="text" required placeholder="Adress"/>
-                <button type="submit">Let's roll</button>
+                <h1>Är du redo för Namnamnam?</h1>
+                <input ref={(input) => { this.inputText = input; }} type="text" required placeholder="Adress"/>
+                <button type="submit" onClick={this.restaurang}>Let's roll</button>
             </form>
         )
     }
