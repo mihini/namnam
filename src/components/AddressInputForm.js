992e8c96dc48b3ababa23aaaf37eca35d26782a6
Nina Juda <nina.juda@gmail.com>
2017-01-20 10:43:28 +0100

    Fix: added required to input

--- src/components/AddressInputForm.js
+++ src/components/AddressInputForm.js
@@ -8,7 +8,7 @@ class AddressInputForm extends React.Component {
           <form className="input-form" onSubmit={this._getGeoData.bind(this)}>
             <label>Namnam hjälper dig att hitta en restaurang i närheten</label>
               <div className="input-area">
-                <input placeholder="Skriv din adress tex. Sveavägen 83, Stockholm..." ref={(input) => this._address = input}/>
+                <input required placeholder="Skriv din adress tex. Sveavägen 83, Stockholm..." ref={(input) => this._address = input}/>
                 <button type="submit">sök</button>
               </div>
             </form>
