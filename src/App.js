d75156d558745c7481f601efc799902942e3fa51
Nina Juda <nina.juda@gmail.com>
2017-01-25 14:55:32 +0100

    Fix: update on app.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,33 @@ import axios from 'axios';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import TextResult from './components/TextResult';
-import Progressbar from './components/Progressbar';
-import Searchbar from './components/Searchbar';
 import AddressInputForm from './components/AddressInputForm';
-import MapResult from './components/MapResult';
-
-var restaurant;
+import Progressbar from './components/Progressbar';
+import SearchResults from './components/SearchResults';
 
 class App extends Component {
   constructor() {
     super();
+
     this._getRestaurant = this._getRestaurant.bind(this);
+    this._getRandomRestaurant = this._getRandomRestaurant.bind(this);
     // this.getGeoUserAddress = this.getGeoUserAddress.bind(this);
+
+    this.state = {
+      showProgressbar: false,
+      showResults: false,
+      restaurant:{}
+    };
   }
 
-  _getRestaurant(location){
+
+  _getRestaurant(location) {
+    this.setState({
+      showProgressbar: !this.state.showProgressbar
+    });
+
+
+    const appComponent = this;
     var lat =location.lat;//data from google api
     var lng = location.lng;
     axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
@@ -32,35 +43,42 @@ class App extends Component {
 
       }
     }).then(function(response){
-        let restaurants = [];
+      appComponent.setState({restaurant: appComponent._getRandomRestaurant(response)});
 
-        for (var i=0;i<response.data.results.length;i++){
-            restaurants.push(response.data.results[i]);
-        }
-        //console.log(response);
-
-        restaurant = restaurants[Math.floor(Math.random() * restaurants.length)];
-        console.log(restaurant.name);
-        console.log(restaurant.geometry.location.lat);
-        console.log(restaurant.geometry.location.lng);
-        
     });
+  } // när vi har fått tillbaka resultatet så är laddningen klar och då sätter
+
+
+  _getRandomRestaurant(response){
+
+    let restaurants = response.data.results;
+    // console.log(restaurants);
+
+    let restaurantObj = restaurants[Math.floor(Math.random() * restaurants.length)];
+    // console.log(restaurantObj);
+
+    this.setState({
+      showResults: !this.state.showResults,
+      showProgressbar: this.state.showResults
+    });
+    return restaurantObj;
   }
 
   render() {
-      return (
-          <div className="App">
-              <Header/>
-              <AddressInputForm getRestaurant={this._getRestaurant}/>
-              <Progressbar/>
-              <TextResult/>
-              <MapResult/>
-              <Footer/>
+    return (
+        <div className="App">
+          <Header/>
+          <AddressInputForm getRestaurant={this._getRestaurant}/>
+          <Progressbar showProgressbar={this.state.showProgressbar}/>
+          <SearchResults showResults={this.state.showResults} restaurantObj={this.state.restaurant}/>
 
-          </div>
-      );
+          {/* showResults blir props i själva componenten (progressbar, jag har döpt dom här, skickar med ett värde )*/}
+
+          <Footer/>
+
+        </div>
+    );
   }
-
 }
 
 
