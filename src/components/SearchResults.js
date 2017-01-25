241e2ba03dbc89396aa6e97944da250bea9594f9
Nina Juda <nina.juda@gmail.com>
2017-01-25 14:57:32 +0100

    Add: Searchresults

---
+++ src/components/SearchResults.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import TextResult from './TextResult';
+import MapResult from './MapResult';
+
+class SearchResults extends React.Component{
+
+
+    render(){
+
+        var show = {
+            display: 'block'
+        }
+
+        var hide = {
+            display: 'none'
+        }
+
+        var showStyle = this.props.showResults ? show : hide;
+
+        return(
+
+            <div className="results" style={showStyle}>
+                <TextResult restaurantObj={this.props.restaurantObj} />
+                <MapResult />
+            </div>
+        )
+    }
+}
+
+export default SearchResults;
