83147842dc87f110a56e1635da712cf5a5ba2e6f
Nina Ma <ninam88@gmail.com>
2017-01-25 11:16:50 +0100

    random function in app.js. textResult done

--- src/App.js
+++ src/App.js
@@ -13,11 +13,18 @@ import MapResult from './components/MapResult';
 class App extends Component {
   constructor() {
     super();
+
+    this.state={
+      restaurant:{}
+    }
+
     this._getRestaurant = this._getRestaurant.bind(this);
+    this._getRandomRestaurant = this._getRandomRestaurant.bind(this);
     // this.getGeoUserAddress = this.getGeoUserAddress.bind(this);
   }
 
   _getRestaurant(location){
+    const appComponent = this;
     var lat =location.lat;//data from google api
     var lng = location.lng;
     axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
@@ -29,8 +36,31 @@ class App extends Component {
 
       }
     }).then(function(response){
-      console.log(response);
+      appComponent.setState({restaurant: appComponent._getRandomRestaurant(response)});
+
+      //
+      //
+      // for (var i=0;i<response.data.results.length;i++){
+      //   restaurants.push(response.data.results[i]);
+      // }
+      // //console.log(response);
+      //
+      // console.log(restaurant.name);
+      // console.log(restaurant.geometry.location.lat);
+      // console.log(restaurant.geometry.location.lng);
     });
+  }
+
+
+  _getRandomRestaurant(response){
+    // console.log('hej');
+    // console.log();
+    let restaurants = response.data.results;
+    // console.log(restaurants);
+
+    let restaurantObj = restaurants[Math.floor(Math.random() * restaurants.length)];
+    // console.log(restaurantObj);
+    return restaurantObj;
   }
 
   render() {
@@ -39,7 +69,7 @@ class App extends Component {
               <Header/>
               <AddressInputForm getRestaurant={this._getRestaurant}/>
               <Progressbar/>
-              <TextResult/>
+              <TextResult restaurantObj={this.state.restaurant}/>
               <MapResult/>
               <Footer/>
 
--- src/components/TextResult.js
+++ src/components/TextResult.js
@@ -1,23 +1,15 @@
 import React from 'react';
 
 class TextResult extends React.Component{
-  // constructor(){
-  //   super();
-  //   this.getRandomRestaurant = this.getRandomRestaurant.bind(this);
-  // }
-
-// function should be in searchform?
-  _getRandomRestaurant(){
-    const restaurants = ["Chopsticks", "Mc donalds", "Något", "jensens", "nybro"];
-
-    const selectedRestaurant = restaurants[Math.floor(Math.random() * restaurants.length)];
-    console.log(this);
-    return selectedRestaurant;
+  constructor(){
+    super();
+    // this.getRandomRestaurant = this.getRandomRestaurant.bind(this);
   }
+
 
   render(){
     return(
-      <h3>{this._getRandomRestaurant()}</h3>
+      <h3>{this.props.restaurantObj.name}</h3>
     )
   }
 }
