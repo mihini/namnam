40d7a237430b356f26689e95c409411db57621ed
mihini <ninam88@gmail.com>
2017-01-26 19:06:17 +0100

    fixed lat and lng for map

--- src/components/MapResult.js
+++ src/components/MapResult.js
@@ -4,22 +4,22 @@ import GoogleMap from 'google-map-react';
 import Marker from './Marker';
 
 class MapResult extends React.Component{
+
   static defaultProps = {
-    center: {lat: 59.2833906, lng: 17.9094205}, // result from google places api's location should be put here
     zoom: 15,
   };
-
   render(){
+
     return(
       <div style={{height: 400}}>
+        <h1></h1>
         <GoogleMap
           bootstrapURLKeys={{key: "AIzaSyCyYN3NAc4dMKq7PqupmsXkMd__yNYaT5s"}}
-          defaultCenter={this.props.center}
+          center={{lat: this.props.restaurantObj.geometry.location.lat, lng: this.props.restaurantObj.geometry.location.lng}}
           defaultZoom={this.props.zoom}>
-
-          <Marker lat={this.props.center.lat} lng={this.props.center.lng}/>
+          <Marker lat={this.props.restaurantObj.geometry.location.lat} lng={this.props.restaurantObj.geometry.location.lng}/>
+          {/*{console.log(this.props.restaurantObj.geometry.location)}*/}
         </GoogleMap>
-
 
     </div>
     )
