92947a3f1aaf6dab6891d5454c898bdb7f9e4973
mihini <ninam88@gmail.com>
2017-02-02 11:25:15 +0100

    added travis file

--- src/test/AddressInputForm.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import AddressInputForm from '../components/AddressInputForm'
-
-it('calls the _getGeoUserAddress function when search button is clicked', () => {
-  component.find('button').simulate('click');
-  expect(_getGeoUserAddress).toBeCalled();
-});
